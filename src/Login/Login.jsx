c8779b75b19b6e657f84f284713d1af58ee74b65
Rodion Zlobin <rodion.zlobin@so4it.com>
2017-07-20 16:06:50 +0200

    update login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -8,23 +8,57 @@ import React from "react";
 
 export default class Login extends React.Component {
 
-    //state user
-    //state password
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userName: "",//React.PropTypes.string,
+            password: "",//React.PropTypes.string,
+            attempt: 1,
+
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleUserNameInput = this.handleUserNameInput.bind(this);
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    }
+
+    handleClick(event){
+        event.preventDefault();
+
+        if(this.state.userName === "1" && this.state.password === "2"){
+            window.location.assign("/service");
+        }
+        else
+        {
+            this.setState({userName: "", password: "", attempt: this.state.attempt + 1});
+        }
+
+    }
+
+    handleUserNameInput(event){
+        this.setState({userName: event.target.value});
+    }
+
+    handlePasswordInput(event){
+        this.setState({password: event.target.value});
+    }
 
 
     render() {
         return <div className="login">
-            <h1>Login</h1>
-            <form action="/service">
-                <input type="text" name="u" placeholder="Username" required="required"/>
-                <input type="password" name="p" placeholder="Password" required="required"/>
+            <h1 className="s-color">Login</h1>
+            <span>Attempt {this.state.attempt}</span>
 
-                <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
+            <form onSubmit={this.handleClick}>
+                <input type="text" value={this.state.userName} placeholder="Username" required="required" onChange={this.handleUserNameInput}/>
+                <input type="password" value={this.state.password} placeholder="Password" required="required" onChange={this.handlePasswordInput}/>
+
+                <button type="submit" className="btn btn-primary btn-block btn-large" >Let me in.</button>
 
             </form>
         </div>
         /*
-        Here is another version = 1 of login
+         Here is another version = 1 of login
          <div class="login-page">
          <div class="form">
          <form class="login-form">
